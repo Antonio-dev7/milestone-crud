19b75476e602653c6fa47a03d7ac4790b5452237
Antonio-dev7 <antonio.emk28@gmail.com>
2020-11-27 12:16:19 +0000

    delete functionality done!

--- run.py
+++ run.py
@@ -58,6 +58,14 @@ def edit_review(review_id):
     return render_template("edit_review.html", review=review, movies=movies)
 
 
+@app.route("/delete_review/<review_id>")
+def delete_review(review_id):
+    mongo.db.reviews.remove({"_id": ObjectId(review_id)})
+    flash("Task Successfully Deleted")
+    return redirect(url_for("get_reviews"))
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
